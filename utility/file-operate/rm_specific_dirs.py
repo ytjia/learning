18a54d1a396224a407218d7cf6c16d2213472f10
Yitian Jia <ytjia.zju@gmail.com>
2016-10-04 19:35:55 -0400

    Update rm_specific_dirs.py
    增加处理文件夹内不为空时删除方式

--- utility/file-operate/rm_specific_dirs.py
+++ utility/file-operate/rm_specific_dirs.py
@@ -7,12 +7,15 @@
 
 import os
 import sys
+import shutil
 
 def rm_specific_dir(top, dir_name):
     for root, dirs, files in os.walk(top, topdown=False):
         for name in dirs:
             if name == dir_name:
-                os.rmdir(os.path.join(root, name))
+                # os.rmdir(os.path.join(root, name))
+                # 文件夹内不为空时
+                shutil.rmtree(os.path.join(root, name))
 
 
 if __name__ == '__main__':
